3d766df453216aac38dd2e88e653de3f8fc9fd83
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-02-06 13:53:53 -0500

    Add tests, fix test app name collision problem

--- testing/ember-concurrency-v1/tests/_the_-test.ts
+++ testing/ember-concurrency-v1/tests/_the_-test.ts
@@ -1,9 +1,14 @@
 import { module, test } from 'qunit';
 
 import { two } from 'my-addon';
+import { range } from 'my-addon/optional-ember-concurrency-file';
 
 module('v2 addon tests', function (hooks) {
   test('utils work', function (assert) {
     assert.strictEqual(two, 2);
+  });
+
+  test('optional dependency resolves as desired', function (assert) {
+    assert.strictEqual(range, '^1.0.0');
   });
 });
